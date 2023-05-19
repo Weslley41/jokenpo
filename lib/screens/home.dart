ce15fc7dcd8b9211b743634163e54396177789c1
unknown <gbenignorocha@gmail.com>
2023-05-19 12:32:24 -0300

    rules_button created on appbar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import '../components/game_battle.dart';
 import '../components/game_options.dart';
 import '../components/restart_button.dart';
+import '../components/rules_button.dart';
 import '../components/scoreboard.dart';
 
 class HomePage extends StatelessWidget {
@@ -13,25 +14,25 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).primaryColor,
-      appBar: AppBar(
-        centerTitle: true,
-        elevation: 0,
-        title: Text(
-          'Jokenpô',
-          style: Theme.of(context).textTheme.headlineMedium?.copyWith(
-            color: Theme.of(context).secondaryHeaderColor,
+        backgroundColor: Theme.of(context).primaryColor,
+        appBar: AppBar(
+          actions: const [RulesButton()],
+          centerTitle: true,
+          elevation: 0,
+          title: Text(
+            'Jokenpô',
+            style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                  color: Theme.of(context).secondaryHeaderColor,
+                ),
           ),
         ),
-      ),
-      body: const Column(
-        children: [
-          Scoreboard(playerScore: 0, computerScore: 0),
-          GameBattle(),
-          GameOptions(),
-          RestartButton()
-        ],
-      )
-    );
+        body: const Column(
+          children: [
+            Scoreboard(playerScore: 0, computerScore: 0),
+            GameBattle(),
+            GameOptions(),
+            RestartButton()
+          ],
+        ));
   }
 }
